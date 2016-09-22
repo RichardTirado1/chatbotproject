27d2022bdc9509a2dc73563c90fb2d83d7cd6f9d
Richard Tirado <richardtirado1@yahoo.com>
2016-09-22 09:18:14 -0700

    Trying to add a response.

--- chatbot.js
+++ chatbot.js
@@ -2,3 +2,15 @@ function talkButton() {
   document.getElementById("chat-area").textContent =
   document.getElementById("input").value;
 }
+function bobbyResponded() {
+
+}
+
+
+
+
+string bobbyResponded {
+  ("What time is it?");
+  ("How old are you?");
+  ("Are you human?")
+}
