ec462d7f5112710aa21bbb161630d3d5d97c1e66
Richard Tirado <richardtirado1@yahoo.com>
2016-09-23 08:55:42 -0700

    Trying to add responses

--- chatbot.js
+++ chatbot.js
@@ -1,16 +1,17 @@
+var userInput = {
+	"what is your name": My name is Bobby,
+	"how old are you": My birth is unknown,
+	"hello": Hi,
+	"goodmorning": its a bad morning,
+	"what is your last name": I dont have one,
+	"goodbye": bye,
+}
+
 function talkButton() {
   document.getElementById("chat-area").textContent =
   document.getElementById("input").value;
-}
-function bobbyResponded() {
 
-}
+  var userInput = $("#chat-area").val();
+  console.log("userInput is " + userInput);
 
-
-
-
-string bobbyResponded {
-  ("What time is it?");
-  ("How old are you?");
-  ("Are you human?")
 }
