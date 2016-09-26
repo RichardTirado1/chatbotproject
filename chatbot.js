4693b8b72be64c45913a7b17a04b86aeb6d16f8b
Richard Tirado <richardtirado1@yahoo.com>
2016-09-26 10:35:03 -0700

    I added chat history

--- chatbot.js
+++ chatbot.js
@@ -8,18 +8,13 @@ var responses = {
   "Who are you?": "I am Bobby",
   "Do you have feelings?": "maybe",
   "Tell me three things about yourself?": "I am Bobby, I am Bobby, I am Bobby",
-  "What is one detail you recall from your last chat?": "",
+  "What is one detail you recall from your last chat?": "You asked me a question.",
 }
 
 function talkButton() {
-  document.getElementById("chat-area").textContent =
-  document.getElementById("input").value;
-
   var userInput = $("#input").val();
-  console.log("userInput is " + userInput);
   var answer = responses[userInput];
-  document.getElementById("chat-area").textContent = answer;
 
-  $('#chat-area').prepend("Bobby: " + ".");
+  $('#chat-area').prepend("Bobby: " + answer + "." + "</br>");
   $('#chat-area').prepend("You: " + userInput + "." + "</br>");
 }
